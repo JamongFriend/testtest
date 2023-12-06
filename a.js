fe04fba47c1d72b8cc5d35a4c49c2dbdef759dab
JamogFrinds <ghrb6769@gmail.com>
2023-12-06 23:06:17 +0900

    first commit

---
+++ a.js
@@ -0,0 +1,23 @@
+function Menu(name){
+  this.name = name;
+}
+
+const coffeeMenu = function(name, price, size){
+  this.price = price;
+  this.size = size;
+  Menu.call(this, name);
+}
+
+const breadMenu = function(name, price){
+  this.price = price;
+  Menu.call(this, name);
+}
+
+coffeeMenu.prototype = new Menu();
+breadMenu.prototype = new Menu();
+
+const Americano = new coffeeMenu("Americano", 2000, "ragular");
+console.log(Americano);
+
+const CheezeBread = new breadMenu("CheezeBread", 4000);
+console.log(CheezeBread);
